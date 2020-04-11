3680c35ba3f6d271719ddd85b74259a33e81c35e
Justin Peckner <pecknerj@gmail.com>
2020-04-11 11:57:41 -0700

    Updates Slather-related scripts

--- fastlane/Lanes/PlacesFinder/build_placesfinder.rb
+++ fastlane/Lanes/PlacesFinder/build_placesfinder.rb
@@ -3,7 +3,7 @@ def generate_placesfinder(baseURL, apiKey, config = "Release")
   generate_config(baseURL, apiKey)
   generate_coordinode_files()
   generate_sourcery_files()
-  generate_project_files(config)
+  generate_project_files()
 end
 
 def generate_config(baseURL, apiKey)
@@ -37,12 +37,12 @@ def generate_sourcery_files()
   )
 end
 
-def generate_project_files(config)
+def generate_project_files()
   run_script(
     [
       "cd Lanes/PlacesFinder",
       "chmod u+x generate_project_files.sh",
-      "./generate_project_files.sh '%{config}'" % {config: config},
+      "./generate_project_files.sh",
     ].join("\n")
   )
 end
