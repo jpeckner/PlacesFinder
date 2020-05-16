f5f15e9e8704f49cf6f05c5fc8bc57db33b4ccc3
Justin Peckner <pecknerj@gmail.com>
2020-05-16 14:48:00 -0700

    Updates to use CoordiNode as a Carthage dependency instead of a submodule

--- fastlane/Lanes/PlacesFinder/generate_coordinode_files.sh
+++ fastlane/Lanes/PlacesFinder/generate_coordinode_files.sh
@@ -3,13 +3,12 @@
 set -e
 
 cd ../../..
+GENERATOR_PATH=$(pwd)/PlacesFinder/Carthage/Checkouts/CoordiNode/CoordiNode/Resources/CoordiNodeGenerator
 COORDINODE_DIR=$(pwd)/PlacesFinder/PlacesFinder/CoordiNode
 OUTPUT_DIR="$COORDINODE_DIR/Output"
 rm -rf "$OUTPUT_DIR"
 mkdir "$OUTPUT_DIR"
 
-cd CoordiNode/BuildScripts
-chmod +x coordinode_generator.sh
-./coordinode_generator.sh                   \
+$GENERATOR_PATH                             \
   "$COORDINODE_DIR/ModuleStructure.yml"     \
   "$OUTPUT_DIR"
